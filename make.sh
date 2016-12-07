370dbe54a1235888ccb2ef10fde521efc63e4e50
Benton Roberts <broberts@mdsol.com>
2016-12-07 08:00:45 -0800

    use golang 1.7

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ echo "Building static linux binary for gosieve..."
 mkdir -p pkg
 buildcmd='CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o pkg/gosieve'
 docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" \
-  -w /app golang:1.6 sh -c "$buildcmd"
+  -w /app golang:1.7 sh -c "$buildcmd"
 
 echo "Building docker image for gosieve..."
 docker build --no-cache=true --tag benton/gosieve .
